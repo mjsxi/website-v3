1d617a450c412064550aa1f8d6a5600abaf574c2
Matt Sullivan <matt.sullivan@sbnation.com>
2017-07-04 11:39:04 -0400

    Broke out js to fix a fatal error

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,61 +1,5 @@
 $( function() {
 
-  // Nav
-
-  var within = 0;
-  var wasWithin = null;
-
-  var tops = [
-    $('[data-section="about"]').position().top,
-    $('[data-section="concert"]').position().top,
-    $('[data-section="insights"]').position().top,
-    $('[data-section="hymnal"]').position().top,
-    $('[data-section="vox"]').position().top,
-    
-  ];
-
-  var $menuItems = $('.nav-items');
-
-  function followMenu() {
-    var top = $(window).scrollTop();
-    for(var i = 0; i < tops.length; i++) {
-      if(top < tops[i] && top > 0) {
-        within = i - 1;
-        break;
-      } else if(top > tops[i] && i == tops.length - 1) {
-        within = tops.length - 1;
-      }
-    }
-    if(within !== wasWithin) {
-      $menuItems.removeClass('nav--active').eq(within).addClass('nav--active');
-      wasWithin = within;
-    }
-  }
-
-  function setTops() {
-    tops = [
-      $('[data-section="about"]').position().top,
-      $('[data-section="concert"]').position().top,
-      $('[data-section="insights"]').position().top,
-      $('[data-section="hymnal"]').position().top,
-      $('[data-section="vox"]').position().top,
-      
-    ];
-  }
-
-  $menuItems.on('click', function(e) {
-    var $section = $('[data-section="' + $(this).attr('data-control') + '"]');
-    $('body,html').animate({
-      scrollTop: $section.find('[data-here]').position().top - window.innerHeight*0.15
-    }, '200', 'swing', function() { });
-  });
-
-
-  $(window).scroll(followMenu);
-  $(window).resize(setTops);
-
-  followMenu();
-
   // Images
 
   function setImageLogic() {
@@ -72,25 +16,25 @@ $( function() {
     // A) ESC: Close all active
     // B) Forward Arrow: Move to next
     // C) Back Arrow: Move to previous
-    // window.addEventListener('keydown', function(e) {
-    //   var active = $('.active');
-    //   if (e.keyCode === 27) {
-    //     active.removeClass('active');
-    //   } else if (
-    //     (e.keyCode === 37 && active.prev().length > 0) ||
-    //     (e.keyCode === 39 && active.next().length > 0)
-    //   ) {
-    //     var target = e.keyCode === 37 ? active.prev() : active.next();
-    //     active.removeClass('active');
-    //     target.addClass('active');
-    //     centerCurrentImage(target);
+    window.addEventListener('keydown', function(e) {
+      var active = $('.active');
+      if (e.keyCode === 27) {
+        active.removeClass('active');
+      } else if (
+        (e.keyCode === 37 && active.prev().length > 0) ||
+        (e.keyCode === 39 && active.next().length > 0)
+      ) {
+        var target = e.keyCode === 37 ? active.prev() : active.next();
+        active.removeClass('active');
+        target.addClass('active');
+        centerCurrentImage(target);
 
-    //     // If we're moving forward, expand the list to mock infinite scrolling
-    //     if (e.keyCode === 39) {
-    //       expandList(target);
-    //     }
-    //   }
-    // });
+        // If we're moving forward, expand the list to mock infinite scrolling
+        if (e.keyCode === 39) {
+          expandList(target);
+        }
+      }
+    });
   }
 
   // If nearing the end of the current image list, clone and expand to create
--- js/main.js
+++ js/main.js
@@ -3,54 +3,6 @@
 
 $( function() {
 
-  // Nav
-
-  var within = 0;
-  var wasWithin = null;
-
-  var tops = [
-    {% for post in site.posts reversed %}$('[data-section="{{ post.title | escape }}"]').position().top,
-    {% endfor %}
-  ];
-
-  var $menuItems = $('.nav-items');
-
-  function followMenu() {
-    var top = $(window).scrollTop();
-    for(var i = 0; i < tops.length; i++) {
-      if(top < tops[i] && top > 0) {
-        within = i - 1;
-        break;
-      } else if(top > tops[i] && i == tops.length - 1) {
-        within = tops.length - 1;
-      }
-    }
-    if(within !== wasWithin) {
-      $menuItems.removeClass('nav--active').eq(within).addClass('nav--active');
-      wasWithin = within;
-    }
-  }
-
-  function setTops() {
-    tops = [
-      {% for post in site.posts reversed %}$('[data-section="{{ post.title | escape }}"]').position().top,
-      {% endfor %}
-    ];
-  }
-
-  $menuItems.on('click', function(e) {
-    var $section = $('[data-section="' + $(this).attr('data-control') + '"]');
-    $('body,html').animate({
-      scrollTop: $section.find('[data-here]').position().top - window.innerHeight*0.15
-    }, '200', 'swing', function() { });
-  });
-
-
-  $(window).scroll(followMenu);
-  $(window).resize(setTops);
-
-  followMenu();
-
   // Images
 
   function setImageLogic() {
@@ -67,25 +19,25 @@ $( function() {
     // A) ESC: Close all active
     // B) Forward Arrow: Move to next
     // C) Back Arrow: Move to previous
-    // window.addEventListener('keydown', function(e) {
-    //   var active = $('.active');
-    //   if (e.keyCode === 27) {
-    //     active.removeClass('active');
-    //   } else if (
-    //     (e.keyCode === 37 && active.prev().length > 0) ||
-    //     (e.keyCode === 39 && active.next().length > 0)
-    //   ) {
-    //     var target = e.keyCode === 37 ? active.prev() : active.next();
-    //     active.removeClass('active');
-    //     target.addClass('active');
-    //     centerCurrentImage(target);
+    window.addEventListener('keydown', function(e) {
+      var active = $('.active');
+      if (e.keyCode === 27) {
+        active.removeClass('active');
+      } else if (
+        (e.keyCode === 37 && active.prev().length > 0) ||
+        (e.keyCode === 39 && active.next().length > 0)
+      ) {
+        var target = e.keyCode === 37 ? active.prev() : active.next();
+        active.removeClass('active');
+        target.addClass('active');
+        centerCurrentImage(target);
 
-    //     // If we're moving forward, expand the list to mock infinite scrolling
-    //     if (e.keyCode === 39) {
-    //       expandList(target);
-    //     }
-    //   }
-    // });
+        // If we're moving forward, expand the list to mock infinite scrolling
+        if (e.keyCode === 39) {
+          expandList(target);
+        }
+      }
+    });
   }
 
   // If nearing the end of the current image list, clone and expand to create
